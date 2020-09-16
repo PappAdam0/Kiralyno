b29f2ee502c052e8edb84faa0c2b9e6554c62a7c
PappAdam0 <papp.adaam@gmail.com>
2020-09-16 08:28:02 +0200

    Add project files.

---
+++ Kiralyno/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralyno
+{
+    class Tabla
+    {
+        char[,] tomb = new char[8,8];
+        char UresCella;
+        int UresOszlopokSzama;
+        int UresSorokSzama;
+
+        public void Elhelyez()
+        {
+
+        }
+        public void FajlbaIr()
+        {
+
+        }
+        public void Megjelenit()
+        {
+
+        }
+        public void Table()
+        {
+
+        }
+        public int UresOszlop()
+        {
+
+        }
+        public int UresSor()
+        {
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
